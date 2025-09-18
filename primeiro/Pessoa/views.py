1072b97bf4842dc4145847f7d1965c100943c631
Paola5858 <paolasesi351@email.com>
2025-09-18 10:44:34 -0300

    corrige a função de cadastro para incluir a renderização do template e adiciona o formulário de cadastro

--- primeiro/Pessoa/views.py
+++ primeiro/Pessoa/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Pessoa
 
 def Cadastro(request):
-    if request.method == "GET"
+    if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
         nome = request.POST.get('nome')
