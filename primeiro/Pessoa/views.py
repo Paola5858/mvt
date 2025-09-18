47b49059539ea58e9504351586ba16a047748631
Paola5858 <paolasesi351@email.com>
2025-09-18 10:19:54 -0300

    registra modelo 'Pessoa' no admin e implementa a função de cadastro

--- primeiro/Pessoa/views.py
+++ primeiro/Pessoa/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pessoa
 
-# Create your views here.
+def Cadastro(request):
+    if request.method == "GET"
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+        email = request.POST.get('email')
+
+        pessoa = Pessoa(nome=nome, idade=idade, email=email)
+        pessoa.save()
+
+        return HttpResponse("Pessoa cadastrada com sucesso!")
