a9a04d1f530d0e9f2b82d2030e89933b3d8b61c4
Paola Soares Machado <paolasesi351@gmail.com>
2026-02-27 08:41:08 -0300

    feat: Atualiza documentação Swagger para os ViewSets da API de Telemetria com resumos e tags

--- telemetria/api_telemetria/views.py
+++ telemetria/api_telemetria/views.py
@@ -19,36 +19,55 @@ class MarcaViewSet(viewsets.ModelViewSet):
     ordering = ['nome']
 
     @swagger_auto_schema(
+        operation_summary="Listar marcas",
         operation_description="Lista todas as marcas cadastradas",
-        responses={200: MarcaSerializer(many=True)}
+        responses={200: MarcaSerializer(many=True)},
+        tags=['Marcas']
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Criar marca",
         operation_description="Cadastra uma nova marca",
-        responses={201: MarcaSerializer()}
+        responses={201: MarcaSerializer()},
+        tags=['Marcas']
     )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Detalhar marca",
         operation_description="Retorna uma marca conforme o ID informado",
-        responses={200: MarcaSerializer()}
+        responses={200: MarcaSerializer()},
+        tags=['Marcas']
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar marca",
         operation_description="Atualiza dados de uma marca conforme o ID informado",
-        responses={200: MarcaSerializer()}
+        responses={200: MarcaSerializer()},
+        tags=['Marcas']
     )
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar marca parcialmente",
+        operation_description="Atualiza parcialmente os dados de uma marca conforme o ID informado",
+        responses={200: MarcaSerializer()},
+        tags=['Marcas']
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Remover marca",
         operation_description="Remove uma marca conforme o ID informado",
-        responses={204: "Marca removida com sucesso"}
+        responses={204: "Marca removida com sucesso"},
+        tags=['Marcas']
     )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
@@ -64,36 +83,55 @@ class ModeloViewSet(viewsets.ModelViewSet):
     ordering = ['nome']
 
     @swagger_auto_schema(
+        operation_summary="Listar modelos",
         operation_description="Lista todos os modelos cadastrados",
-        responses={200: ModeloSerializer(many=True)}
+        responses={200: ModeloSerializer(many=True)},
+        tags=['Modelos']
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Criar modelo",
         operation_description="Cadastra um novo modelo",
-        responses={201: ModeloSerializer()}
+        responses={201: ModeloSerializer()},
+        tags=['Modelos']
     )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Detalhar modelo",
         operation_description="Retorna um modelo conforme o ID informado",
-        responses={200: ModeloSerializer()}
+        responses={200: ModeloSerializer()},
+        tags=['Modelos']
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar modelo",
         operation_description="Atualiza dados de um modelo conforme o ID informado",
-        responses={200: ModeloSerializer()}
+        responses={200: ModeloSerializer()},
+        tags=['Modelos']
     )
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar modelo parcialmente",
+        operation_description="Atualiza parcialmente os dados de um modelo conforme o ID informado",
+        responses={200: ModeloSerializer()},
+        tags=['Modelos']
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Remover modelo",
         operation_description="Remove um modelo conforme o ID informado",
-        responses={204: "Modelo removido com sucesso"}
+        responses={204: "Modelo removido com sucesso"},
+        tags=['Modelos']
     )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
@@ -110,36 +148,55 @@ class VeiculoViewSet(viewsets.ModelViewSet):
     ordering = ['-ano']
 
     @swagger_auto_schema(
+        operation_summary="Listar veículos",
         operation_description="Lista todos os veículos cadastrados",
-        responses={200: VeiculoSerializer(many=True)}
+        responses={200: VeiculoSerializer(many=True)},
+        tags=['Veículos']
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Criar veículo",
         operation_description="Cadastra um novo veículo com marca, modelo, ano e horímetro",
-        responses={201: VeiculoSerializer()}
+        responses={201: VeiculoSerializer()},
+        tags=['Veículos']
     )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Detalhar veículo",
         operation_description="Retorna um veículo conforme o ID informado",
-        responses={200: VeiculoSerializer()}
+        responses={200: VeiculoSerializer()},
+        tags=['Veículos']
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar veículo",
         operation_description="Atualiza dados de um veículo conforme o ID informado",
-        responses={200: VeiculoSerializer()}
+        responses={200: VeiculoSerializer()},
+        tags=['Veículos']
     )
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar veículo parcialmente",
+        operation_description="Atualiza parcialmente os dados de um veículo conforme o ID informado",
+        responses={200: VeiculoSerializer()},
+        tags=['Veículos']
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Remover veículo",
         operation_description="Remove um veículo conforme o ID informado",
-        responses={204: "Veículo removido com sucesso"}
+        responses={204: "Veículo removido com sucesso"},
+        tags=['Veículos']
     )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
@@ -153,36 +210,55 @@ class UnidadeMedidaViewSet(viewsets.ModelViewSet):
     search_fields = ['nome']
 
     @swagger_auto_schema(
+        operation_summary="Listar unidades de medida",
         operation_description="Lista todas as unidades de medida cadastradas",
-        responses={200: UnidadeMedidaSerializer(many=True)}
+        responses={200: UnidadeMedidaSerializer(many=True)},
+        tags=['Unidades de Medida']
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Criar unidade de medida",
         operation_description="Cadastra uma nova unidade de medida",
-        responses={201: UnidadeMedidaSerializer()}
+        responses={201: UnidadeMedidaSerializer()},
+        tags=['Unidades de Medida']
     )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Detalhar unidade de medida",
         operation_description="Retorna uma unidade de medida conforme o ID informado",
-        responses={200: UnidadeMedidaSerializer()}
+        responses={200: UnidadeMedidaSerializer()},
+        tags=['Unidades de Medida']
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar unidade de medida",
         operation_description="Atualiza dados de uma unidade de medida conforme o ID informado",
-        responses={200: UnidadeMedidaSerializer()}
+        responses={200: UnidadeMedidaSerializer()},
+        tags=['Unidades de Medida']
     )
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar unidade de medida parcialmente",
+        operation_description="Atualiza parcialmente os dados de uma unidade de medida conforme o ID informado",
+        responses={200: UnidadeMedidaSerializer()},
+        tags=['Unidades de Medida']
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Remover unidade de medida",
         operation_description="Remove uma unidade de medida conforme o ID informado",
-        responses={204: "Unidade de medida removida com sucesso"}
+        responses={204: "Unidade de medida removida com sucesso"},
+        tags=['Unidades de Medida']
     )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
@@ -197,36 +273,55 @@ class MedicaoViewSet(viewsets.ModelViewSet):
     search_fields = ['tipo']
 
     @swagger_auto_schema(
+        operation_summary="Listar medições",
         operation_description="Lista todas as medições cadastradas",
-        responses={200: MedicaoSerializer(many=True)}
+        responses={200: MedicaoSerializer(many=True)},
+        tags=['Medições']
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Criar medição",
         operation_description="Cadastra uma nova medição com tipo e unidade de medida",
-        responses={201: MedicaoSerializer()}
+        responses={201: MedicaoSerializer()},
+        tags=['Medições']
     )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Detalhar medição",
         operation_description="Retorna uma medição conforme o ID informado",
-        responses={200: MedicaoSerializer()}
+        responses={200: MedicaoSerializer()},
+        tags=['Medições']
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar medição",
         operation_description="Atualiza dados de uma medição conforme o ID informado",
-        responses={200: MedicaoSerializer()}
+        responses={200: MedicaoSerializer()},
+        tags=['Medições']
     )
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar medição parcialmente",
+        operation_description="Atualiza parcialmente os dados de uma medição conforme o ID informado",
+        responses={200: MedicaoSerializer()},
+        tags=['Medições']
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Remover medição",
         operation_description="Remove uma medição conforme o ID informado",
-        responses={204: "Medição removida com sucesso"}
+        responses={204: "Medição removida com sucesso"},
+        tags=['Medições']
     )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
@@ -243,36 +338,55 @@ class MedicaoVeiculoViewSet(viewsets.ModelViewSet):
     ordering = ['-data']
 
     @swagger_auto_schema(
+        operation_summary="Listar medições de veículos",
         operation_description="Lista todos os registros de medição de veículos",
-        responses={200: MedicaoVeiculoSerializer(many=True)}
+        responses={200: MedicaoVeiculoSerializer(many=True)},
+        tags=['Medições de Veículos']
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Criar medição de veículo",
         operation_description="Registra uma nova medição para um veículo",
-        responses={201: MedicaoVeiculoSerializer()}
+        responses={201: MedicaoVeiculoSerializer()},
+        tags=['Medições de Veículos']
     )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Detalhar medição de veículo",
         operation_description="Retorna um registro de medição conforme o ID informado",
-        responses={200: MedicaoVeiculoSerializer()}
+        responses={200: MedicaoVeiculoSerializer()},
+        tags=['Medições de Veículos']
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar medição de veículo",
         operation_description="Atualiza um registro de medição conforme o ID informado",
-        responses={200: MedicaoVeiculoSerializer()}
+        responses={200: MedicaoVeiculoSerializer()},
+        tags=['Medições de Veículos']
     )
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
+        operation_summary="Atualizar medição de veículo parcialmente",
+        operation_description="Atualiza parcialmente um registro de medição conforme o ID informado",
+        responses={200: MedicaoVeiculoSerializer()},
+        tags=['Medições de Veículos']
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Remover medição de veículo",
         operation_description="Remove um registro de medição conforme o ID informado",
-        responses={204: "Registro removido com sucesso"}
+        responses={204: "Registro removido com sucesso"},
+        tags=['Medições de Veículos']
     )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
